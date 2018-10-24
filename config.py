ff599be4121566e74dcb95e6f62ed567a601617a
dudycooly <jay.naveen@gmail.com>
2018-10-24 09:46:43 +0100

    Rename parent

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from os.path import abspath, dirname, join
 _cwd = dirname(abspath(__file__))
 
 SECRET_KEY = 'flask-session-insecure-secret-key'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + join(_cwd, 'flask-tracking.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + join(_cwd, 'site-tracking.db')
 SQLALCHEMY_ECHO = True
